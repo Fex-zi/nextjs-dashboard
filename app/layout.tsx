193efd43d742859e903ad99e89070f4130dc06dd
Fexzi <fexzi87@gmail.com>
2024-02-24 18:43:20 -0700

    More

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,10 @@ import { Metadata } from 'next';
  
 export const metadata: Metadata = {
   title: {
-    template: '%s | Acme Dashboard',
-    default: 'Acme Dashboard',
+    template: '%s | Fexzi Dashboard',
+    default: 'Fexzi Dashboard',
   },
-  description: 'The official Next.js Learn Dashboard built with App Router.',
+  description: 'The official Next.js Dashboard built with App Router.',
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
 };
  
